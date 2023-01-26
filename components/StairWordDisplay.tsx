15b5f34782e5c818a056a9ce704770a5b1e248c6
Christian Kiely <ckiely91@gmail.com>
2023-01-26 19:21:46 +1100

    Prototype

---
+++ components/StairWordDisplay.tsx
@@ -0,0 +1,112 @@
+import { FC, useMemo } from "react";
+
+export const getOverlapCountBetweenWords = (wordA: string, wordB: string) => {
+  for (let i = 1; i < wordA.length; i++) {
+    const sliced = wordA.slice(i);
+    if (wordB.startsWith(sliced)) {
+      return sliced.length;
+    }
+  }
+
+  return 0;
+};
+
+export const getOverlapScore = (words: string[]) => {
+  let score = 0;
+
+  for (let i = 1; i < words.length; i++) {
+    score += getOverlapCountBetweenWords(words[i - 1], words[i]);
+  }
+
+  return score;
+};
+
+interface IStairWordDisplayProps {
+  words: string[];
+  highlightRow?: number;
+}
+
+const StairWordDisplay: FC<IStairWordDisplayProps> = ({
+  words,
+  highlightRow,
+}) => {
+  const renderItems = useMemo(() => {
+    let totalLength = 0;
+    let hasBlanks = false;
+
+    return words.map((word, i) => {
+      if (word === "") {
+        hasBlanks = true;
+      }
+
+      const prevWordOverlapCount =
+        i > 0 ? getOverlapCountBetweenWords(words[i - 1], word) : 0;
+      const nextWordOverlapCount =
+        i < words.length - 1
+          ? getOverlapCountBetweenWords(word, words[i + 1])
+          : 0;
+
+      let prevOverlap, bothOverlap, nonOverlap, nextOverlap;
+
+      if (prevWordOverlapCount + nextWordOverlapCount > word.length) {
+        prevOverlap = word.slice(0, word.length - nextWordOverlapCount);
+        bothOverlap = word.slice(
+          word.length - nextWordOverlapCount,
+          prevWordOverlapCount
+        );
+        nonOverlap = "";
+        nextOverlap = word.slice(prevWordOverlapCount);
+      } else {
+        prevOverlap = word.slice(0, prevWordOverlapCount);
+        bothOverlap = "";
+        nonOverlap = word.slice(
+          prevWordOverlapCount,
+          word.length - nextWordOverlapCount
+        );
+        nextOverlap = word.slice(word.length - nextWordOverlapCount);
+      }
+
+      const indent = totalLength - prevWordOverlapCount;
+
+      totalLength += word.length - prevWordOverlapCount;
+
+      return {
+        indent,
+        prevOverlap,
+        nonOverlap,
+        nextOverlap,
+        bothOverlap,
+        rightAlign: hasBlanks && i === words.length - 1,
+      };
+    });
+  }, [words]);
+
+  return (
+    <div className="font-mono text-2xl overflow-x-auto ">
+      <div className="py-2 min-w-full inline-flex flex-col divide-y divide-dotted divide-slate-700">
+        {renderItems.map((item, i) => (
+          <div
+            key={i}
+            className={`py-1 px-5 ${item.rightAlign ? "text-right" : ""} ${
+              highlightRow === i ? "bg-slate-800" : ""
+            }`}
+          >
+            {!item.rightAlign && (
+              <span className="whitespace-pre">{" ".repeat(item.indent)}</span>
+            )}
+            <span className="text-orange-400 whitespace-pre">
+              {item.prevOverlap}
+            </span>
+            <span className="text-orange-400 whitespace-pre">
+              {item.bothOverlap}
+            </span>
+            <span className="whitespace-pre">{item.nonOverlap}</span>
+            <span className=" whitespace-pre">{item.nextOverlap}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StairWordDisplay;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,67 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft, faChartLine } from "@fortawesome/free-solid-svg-icons";
+import { Open_Sans, Cutive_Mono } from "@next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-opensans",
+});
+
+const cutiveMono = Cutive_Mono({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-cutivemono",
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>StairWord</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={`${openSans.variable} ${cutiveMono.variable} font-sans flex justify-center flex-auto dark:bg-slate-900 dark:text-slate-50`}
+      >
+        <div className="w-full max-w-lg flex flex-col">
+          <header className="flex justify-between items-center px-3 border-b-2 border-gray-200">
+            <div className="w-5">
+              {router.pathname !== "/" && (
+                <Link href="/">
+                  <FontAwesomeIcon
+                    icon={faCircleLeft}
+                    className="w-5 cursor-pointer"
+                  />
+                </Link>
+              )}
+            </div>
+            <h1 className="text-4xl font-bold uppercase tracking-wide text-center my-1 flex-auto">
+              StairWord
+            </h1>
+            {/* <div className="w-5">
+              {router.pathname !== "/stats" && (
+                <Link href="/stats">
+                  <FontAwesomeIcon
+                    icon={faChartLine}
+                    className="w-5 cursor-pointer"
+                  />
+                </Link>
+              )}
+            </div> */}
+          </header>
+          <div className="flex flex-grow flex-col relative">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default MyApp;
